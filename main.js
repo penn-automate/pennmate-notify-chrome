c50d066f2bdbe5bfa6407879dfd506fc8dbdf064
SuperFashi <admin@superfashi.com>
2019-04-23 02:02:00 -0400

    Update v0.2. UI update. Added course change time. Added typeahead.

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 (function () {
     'use strict';
     const regex = /^([a-zA-Z]{2,4})\s*-?((?!000)\d{3}|(?!00)\d{2})-?(?!000)(\d{3})$/;
+    let courses;
+    $.get('https://pennmate.com/courses.php', data => courses = data);
 
     const messaging = firebase.messaging();
     messaging.usePublicVapidKey('BBhlsokXAodD8g4ZkKxdUDPRr54_89XvRzzX_GdKH1gWiosh-IGvr2tQzerqsexpRxNhUDYL4PatwOutgeLkAHY');
-    // messaging.getToken().then(console.log);
 
     function check_validity(value) {
         const match = regex.exec(value);
@@ -34,34 +35,45 @@
         c_list.show();
         const course_human = course.replace(' ', '');
         let html = $('<li class="list-group-item course"><div class="row">' +
-            '<div class="col-6">' + course_human + '</div>' +
-            '<div class="col-6"><button type="button" class="btn btn-info btn-sm float-right rounded-circle">' +
+            '<div class="col-5 text-nowrap">' + course_human + '</div>' +
+            '<div class="col-5 text-info abs-center text-nowrap"></div>' +
+            '<div class="col-2"><button type="button" class="btn btn-danger btn-sm btn-block" title="Delete">' +
             '<i class="fa fa-trash-alt fa-sm"></i></button></div>' +
             '</div></li>');
         html.find('button').click(() => {
             let modal = $('<div class="modal fade" tabindex="-1" role="alertdialog">' +
-                '  <div class="modal-dialog modal-dialog-centered" role="document">' +
-                '    <div class="modal-content">' +
-                '      <div class="modal-header">' +
-                '        <h5 class="modal-title">Confirm Deletion</h5>' +
-                '      </div>' +
-                '      <div class="modal-body">' +
-                '        Do you really want to delete course ' + course_human + '?' +
-                '      </div>' +
-                '      <div class="modal-footer">' +
-                '        <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>' +
-                '        <button type="button" class="btn btn-primary">Yes</button>' +
-                '      </div>' +
-                '    </div>' +
-                '  </div>' +
+                '<div class="modal-dialog modal-dialog-centered" role="document">' +
+                '<div class="modal-content">' +
+                '<div class="modal-header">' +
+                '<h5 class="modal-title">Confirm Deletion</h5>' +
+                '</div>' +
+                '<div class="modal-body">Do you really want to delete course ' +
+                '<span class="text-nowrap">' + course_human + '</span>?' +
+                '</div>' +
+                '<div class="modal-footer">' +
+                '<button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>' +
+                '<button type="button" class="btn btn-warning">Yes</button>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
                 '</div>');
-            modal.find('button.btn-primary').click(() => {
+            modal.find('button.btn-warning').click(() => {
                 modal.modal('hide');
                 html.remove();
                 delete_course(course);
             });
             modal.on('hidden.bs.modal', () => modal.remove());
             modal.modal('show');
+        });
+        $.get('https://pennmate.com/last_opened.php', {course: course}, (data) => {
+            if (data === '-1') {
+                html.find('div.text-info').html('Course now opens.');
+            } else if (data) {
+                const date = new Date(data * 1000);
+                const options = {month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric'};
+                html.find('div.text-info').html('Last opened:<br>' +
+                    date.toLocaleDateString('en-US', options));
+            }
         });
         c_list.append(html);
     }
@@ -73,6 +85,11 @@
                 topics: courses.map(x => x.replace(/\s|-/g, s => (s === '-' ? '' : '%'))),
             });
         });
+    }
+
+    function course_typeahead(q, cb) {
+        q = q.replace(/\s|-/g, '').toUpperCase();
+        cb(courses.filter(s => s['id'].startsWith(q)));
     }
 
     $(document).ready(() => {
@@ -99,6 +116,34 @@
             }
             return false;
         });
+        course_input.typeahead({
+                classNames: {
+                    menu: 'list-group',
+                    suggestion: 'list-group-item',
+                    cursor: 'active',
+                },
+                hint: false,
+                highlight: false,
+                minLength: 1,
+            },
+            {
+                name: 'courses',
+                limit: 20,
+                displayKey: data => data.id.replace(regex, '$1-$2-$3'),
+                source: course_typeahead,
+                templates: {
+                    suggestion: (l) => {
+                        return '<a href="#" class="list-group-item-action">' +
+                            '<div class="d-flex w-100 justify-content-between">' +
+                            '<h5 class="mb-0">' + l.id.replace(regex, '$1 $2 $3') + '</h5>' +
+                            '<small>' + l.act + '</small>' +
+                            '</div>' +
+                            '<p class="mb-0">' + l.title + '</p>' +
+                            '<small>' + l.inst.join(', ') + '</small>' +
+                            '</a>';
+                    }
+                }
+            });
         course_input.change(() => {
             const value = course_input.val();
             if (value === '') {
@@ -107,8 +152,10 @@
                 check_validity(value);
             }
         });
-        dialog.on('hidden.bs.modal', () => {
-            course_input.val('');
+        dialog.on('shown.bs.modal', () => {
+            course_input.focus();
+        }).on('hidden.bs.modal', () => {
+            course_input.typeahead('val', '');
             course_input.change();
         });
         chrome.storage.sync.get('course_list', function (item) {
